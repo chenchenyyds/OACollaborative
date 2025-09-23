6132a6926cee38f869696601c4a664ea5c35837d
zzh <1759860491@qq.com>
2025-09-24 02:07:16 +0800

    新增OA模块

---
+++ aicos-module-oa/src/main/java/com/hbqilufc/module/oa/controller/admin/testConller.java
@@ -0,0 +1,10 @@
+package com.hbqilufc.module.oa.controller.admin;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class testConller {
+    public static void main(String[] args) {
+    }
+}
